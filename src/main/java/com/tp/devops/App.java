345f4fec5bedf998ae0c1b569ac2e8ddd6a844fe
Abderrahim Igouzouln <igouzoulnabderrahim@gmail.com>
2026-04-10 12:00:02 +0100

    add

--- src/main/java/com/tp/devops/App.java
+++ src/main/java/com/tp/devops/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        for(int i = 1; i < 10 ; i++){
+            System.out.println( "Hello World!" );
+        }
     }
 }
